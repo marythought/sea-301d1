72f953fca30278255797530d13cbc008a1eafd79
Mary Dickson Diaz <mdickson@gmail.com>
2015-11-24 17:11:10 -0800

    add style

--- blog/blog.js
+++ blog/blog.js
@@ -1,6 +1,6 @@
 // Technical Requirements and Grading Rubric
 //
-// Use good Object Oriented code: Create a blog object, and a constructor function for array.
+// Use good Object Oriented code: Create a blog object, and a constructor function for articles.
 // Leave as little in the window (global) namespace as possible: attach functions to objects, etc.
 // Create the markup for an example Article in the DOM, then use jQuery to clone that for each article.
 // Your Article prototype should have a .toHtml() function that adds new data to the DOM.
@@ -18,10 +18,10 @@ $( document ).ready(function() {
   }
 
   Article.prototype.toHtml = function() {
-    htmlString = '<li class="article"><h1>';
+    htmlString = '<div class="blog-post" class="clearfix"><h2 class="blog-post-title">';
     htmlString += this.title;
-    htmlString += '</h1>';
-    htmlString += '<p>By <a href="';
+    htmlString += '</h2>';
+    htmlString += '<p class="blog-post-meta">By <a href="';
     htmlString += this.authorUrl;
     htmlString += '">';
     htmlString += this.author;
@@ -29,7 +29,7 @@ $( document ).ready(function() {
     htmlString += this.publishedOn;
     htmlString += '</p>';
     htmlString += this.body;
-    htmlString += '</li>';
+    htmlString += '</div>';
     return htmlString;
   };
 
